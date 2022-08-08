0c782531ab190c1e8cecfdc49d46d71c21f139e5
Naomi Cheeves <nccheeves@gmail.com>
2022-08-08 08:13:35 -0700

    Update llnl_job_main.py

--- llnl_job_main.py
+++ llnl_job_main.py
@@ -1,23 +1,78 @@
-https://github.com/techwithtim/Beautiful-Soup-Tutorial.gitfrom bs4 import BeautifulSoup
-import requests
+#!/usr/bin/env python
+# pylint: disable=C0116,W0613
+# This program is dedicated to the public domain under the CC0 license.
 
-# HTML From File
-with open("index.html", "r") as f:
-        doc = BeautifulSoup(f, "html.parser")
+"""
+Simple Bot to reply to Telegram messages.
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
 
-tags = doc.find_all("p")[0]
+import logging
+from llnl_job_scraper import getLLNLJob
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, ContextTypes
+import os
 
-print(tags.find_all("b"))
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
 
-# HTML From Website
-url = "https://www.finishline.com/store/men/shoes/casual/_/store/product/mens-nike-air-force-1-low-casual-shoes/prod795980?styleId=CW2288&colorId=111"
-# "https://www.newegg.ca/gigabyte-geforce-rtx-3080-ti-gv-n308tgaming-oc-12gd/p/N82E16814932436?Description=3080&cm_re=3080-_-14-932-436-_-Product"
+logger = logging.getLogger(__name__)
 
-result = requests.get(url)
-doc = BeautifulSoup(result.text, "html.parser")
 
-prices = doc.find_all(text="$")
-parent = prices[0].parent
-strong = parent.find("strong")
-print(strong.string)
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+async def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="""
+                                   Hi! I'm a bot. I will help you search for jobs at LLNL. Just pass
+                                   me a list of keywords separated by spaces so I can help you find your ideal position.""")
+    
 
+def help(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+# async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#     await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+async def query(update: Update, context: CallbackContext) -> None:
+    job = getLLNLJob(context.args)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=job)
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+
+    updater = Updater("5569737914:AAEsJWDOBn-pxOkxnr4f4a-hx9XovyIOJ58")
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help))
+    dispatcher.add_handler(CommandHandler("query", query))
+
+    # on non command i.e message - echo the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle() 
+
+if __name__ == '__main__':
+    main()
